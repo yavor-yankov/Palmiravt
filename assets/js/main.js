ac52b020b17b82b8a1d2e0761179d86c7a7db951
Yavor Yankov <yavoryankov3@gmail.com>
2023-09-14 20:52:35 +0300

    updateing apartments section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Gp
-* Updated: Aug 30 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
@@ -33,6 +27,7 @@
       }
     }
   }
+
 
   /**
    * Easy on scroll event listener 
